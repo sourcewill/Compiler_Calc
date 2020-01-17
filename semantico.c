fbdcb138bc9d1886fae104aa5d5f38690c257f68
William Rodrigues <email.williamrodrigues@gmail.com>
2020-01-17 13:01:00 -0300

    Operacao MUL para inteiros.

--- semantico.c
+++ semantico.c
@@ -166,6 +166,36 @@ int insere_sub_INT_saida(int reg1, int reg2){
 
 }
 
+/* Insere instrucao de multiplicacao e retorna o registrador que contem o resultado */
+int insere_mul_INT_saida(int reg1, int reg2){
+
+	int novo_reg1 = ID_REG++;
+	int novo_reg2 = ID_REG++;
+	int novo_reg3 = ID_REG++;
+	int reg_resultado = ID_REG++;
+
+	insere_load_INT_saida(reg1, novo_reg1);
+	insere_load_INT_saida(reg2, novo_reg2);
+
+	char* mul_inicio = "\n  %";
+	char* mul_meio = " = mul nsw i32 %";
+	char* mul_fim = ", %";
+
+	fprintf(arq_saida, "%s", mul_inicio);
+	fprintf(arq_saida, "%d", novo_reg3);
+	fprintf(arq_saida, "%s", mul_meio);
+	fprintf(arq_saida, "%d", novo_reg1);
+	fprintf(arq_saida, "%s", mul_fim);
+	fprintf(arq_saida, "%d", novo_reg2);
+
+	insere_alloca_INT_saida(reg_resultado);
+
+	insere_store_INT_regs_saida(novo_reg3, reg_resultado);
+
+	return reg_resultado;
+
+}
+
 
 /* Insere instruao store (registrador -> registrador) */
 void insere_atribuicao_INT_saida(int reg_origem, int reg_destino){
@@ -231,6 +261,16 @@ struct tipo_registrador percorre_expressao(struct no* no){
 				case NUM_INT:
 					tipo_registrador.tipo = NUM_INT;
 					tipo_registrador.registrador = insere_sub_INT_saida(tipo_registrador_l.registrador, tipo_registrador_r.registrador);
+					return tipo_registrador;
+			}
+			break;
+		case MUL:
+			tipo_registrador_l = percorre_expressao(no->l);
+			tipo_registrador_r = percorre_expressao(no->r);
+			switch(tipo_registrador_l.tipo){
+				case NUM_INT:
+					tipo_registrador.tipo = NUM_INT;
+					tipo_registrador.registrador = insere_mul_INT_saida(tipo_registrador_l.registrador, tipo_registrador_r.registrador);
 					return tipo_registrador;
 			}
 			break;
